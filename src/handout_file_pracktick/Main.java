bed838002dd04d5688773f210d697104b0bc7db1
Elmirbek Surkeev <elmirsurkeev@gmail.com>
2024-11-15 00:24:22 +0600

    pracktick stream methods

--- src/handout_file_pracktick/Main.java
+++ src/handout_file_pracktick/Main.java
@@ -1,26 +1,104 @@
 package handout_file_pracktick;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
+
+import static java.util.stream.Collectors.toList;
+//import static java.util.stream.Nodes.collect;
 
 public class Main {
     public static void main(String[] args) {
-        Movies[] movies = JSONFileHandler.getMovies();
+//        Movies[] movies = JSONFileHandler.getMovies();
+//        var movieNames = movies.stream()
+//                .map(movie -> movie.getName());
+//                .collect(toList());
+
+//        var names = new ArrayList<String>(){
+//            for (var movie : movies){
+//                names.add(movie.getName());
+//            }
+//        }
+
+//        var squared = new numbers.stream()
+//                .map( -> num* num)
+//                .collect(toList());
+//
+
+//        var squared = new ArrayList<int[]>();
+//        for (int num : numbers){
+//            squared.add(num*num);
+//        }
+//        var totalBudget = movies.stream()
+//                .mapToDouble(movie -> movie.getBudget())
+//                .sum();
 
 
+//        double sum = 0;
+//        for (int movieBudget : movies){
+//            movieBudget+=sum;
+//        }
 
 
+//        var years = movies.stream(){
+//            .filter(movie -> movie.getYead() == 2019)
+//            .collect(toList());
+//        }
 
-        String text = "Classes to support functional style" +
-                        "operation on streams of element" +
-                        "such as map-reduce transformations" +
-                        "on collection";
+
+//        var filtered = new ArrayList<Movies>();
+//
+//        for (var movie : movies){
+//            if(movie.getYear == 2019){
+//                filtered.add(movie);
+//            }
+//        }
+
+//        var numbers = List.of(4, 5, 5, 2, 6,3 , 2, 23 , 23 );
+//
+//        var unicalNumbers = numbers.stream()
+//                .filter(num -> num % 2 == 0)
+//                .distinct()
+//                .collect(toList());
+//        System.out.println(unicalNumbers);
+
+//        var numbers = List.of(2,4, 2, 5, 12, 453, 53,2);
+//        var unique = new HashSet<>(numbers);
+//        var filtered = new ArrayList<>(numbers);
+//
+//        for (var number : numbers){
+//            if(number %2 == 0){
+//                if(unique.add(number)){
+//                    filtered.add(number);
+//                }
+//            }
+//        }
+//        System.out.println(filtered);
+
+//        String text = "Classes to support functional style" +
+//                        "operation on streams of element" +
+//                        "such as map-reduce transformations" +
+//                        "on collection";
 
 //        Arrays.stream(text.split("\n")
 //                .filter(word -> word.length() < 5)
 //                .forEach(System.out::println);
 //        );
 
+
+//        var listOfListActors = movies.stream()
+//                .map(movie -> movie.getCast())
+//                .collect(toList());
+
+//        var listOfActors = movies.stream()
+//                .flatMap(movie -> movie.getCast().stream())
+//                .collect(toList());
+
+
+//        var actors = new ArrayList<Actor>();
+//        for (var movie : movies){
+//            for (var actor: movie.getCast()){
+//                actors.add(actor);
+//            }
+//        }
 
 //        var random = new Random();
 //        random.ints(1, 10)//рандомные числа
